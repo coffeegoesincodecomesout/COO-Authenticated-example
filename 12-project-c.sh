78bdd14aa3529876625bac476b8ecbe8c0447c0b
Nigel Smith <nigsmith@redhat.com>
2025-09-28 14:23:55 +0100

    changed the user for project-c

--- 12-project-c.sh
+++ 12-project-c.sh
@@ -4,4 +4,4 @@ oc -n project-c create serviceaccount thanos-querier
 oc -n project-c annotate serviceaccount thanos-querier serviceaccounts.openshift.io/oauth-redirectreference.thanos-querier='{"kind":"OAuthRedirectReference","apiVersion":"v1","reference":{"kind":"Route","name":"thanos-querier-authenticated"}}'
 oc -n project-c annotate service thanos-querier-example-coo-thanos service.alpha.openshift.io/serving-cert-secret-name="thanos-tls"
 oc -n project-c create route reencrypt thanos-querier-authenticated --service=thanos-querier-example-coo-thanos --port=proxy --insecure-policy=Redirect
-oc -n project-c adm policy add-role-to-user view user2
+oc -n project-c adm policy add-role-to-user view user3
