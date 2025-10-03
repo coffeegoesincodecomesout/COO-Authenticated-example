da4bd4a06879818d02884d8f2b87acf4103801f0
Nigel Smith <nigsmith@redhat.com>
2025-10-03 09:45:36 +0100

    macos gives error for tr command - replacing with alternative command

--- 09-project-b.sh
+++ 09-project-b.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-oc -n project-b create secret generic thanos-proxy --from-literal=session_secret=$(head /dev/urandom | tr -dc A-Za-z0-9 | head -c43)
+oc -n project-c create secret generic thanos-proxy --from-literal=session_secret=$(head -c 32 /dev/urandom | base64)
 oc -n project-b create serviceaccount thanos-querier
 oc -n project-b annotate serviceaccount thanos-querier serviceaccounts.openshift.io/oauth-redirectreference.thanos-querier='{"kind":"OAuthRedirectReference","apiVersion":"v1","reference":{"kind":"Route","name":"thanos-querier-authenticated"}}'
 oc -n project-b annotate service thanos-querier-example-coo-thanos service.alpha.openshift.io/serving-cert-secret-name="thanos-tls"
